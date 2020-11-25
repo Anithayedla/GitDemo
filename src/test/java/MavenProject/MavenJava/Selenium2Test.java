2ec48b8e9f7d255a4d099f8100661651a348f951
Anithayedla <yedlaanitha23@gmail.com>
2020-11-26 00:59:54 +0530

    first commit for test

---
+++ src/test/java/MavenProject/MavenJava/Selenium2Test.java
@@ -0,0 +1,15 @@
+package MavenProject.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class Selenium2Test {
+	@Test
+	public void prgrm3()
+	{
+		System.out.println("Test3");
+	}
+
+
+}
+
+
---
+++ src/test/java/MavenProject/MavenJava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package MavenProject.MavenJava;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+	@Test
+	public void prgrm1()
+	{
+		System.out.println("Test1");
+	}
+	@Test
+	public void prgrm2()
+	{
+		
+		System.out.println("Test2");
+	}
+
+}
+
+
+
